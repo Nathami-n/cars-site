900d9e33cbb65a3109b3ac559d2ec207f579c33b
Nathan George <georgenathan010@gmail.com>
2024-01-18 11:39:29 +0300

    changes to be made in the whole outline

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import {Box} from '@mui/material'
+import {Box,Stack,Typography} from '@mui/material'
 const Home = () => {
   return (
     <Box  sx={{minHeight:'85vh'}}>
       
-
+    <Typography variant='h4' sx={{color:'red', wordWrap:'break-word'}}>
+      Want the best of the Best?
+      You are in the right place
+    </Typography>
     </Box>
   )
 }

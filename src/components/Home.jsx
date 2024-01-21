58d29dd58df1eccda2bc5fdfd5767436b04d0658
Nathan George <georgenathan010@gmail.com>
2024-01-21 16:17:42 +0300

    added a placeholder carousel

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import {Cards} from './'
 import {Box,Stack,Typography} from '@mui/material'
+import { images } from '../utils/images'
 const Home = () => {
   return (
     <><Box sx={{ minHeight: '80vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <Cards />
+      <Cards images = {images} />
 
       <Typography variant='h4' sx={{ color: 'red', wordWrap: 'break-word' }}>
 
